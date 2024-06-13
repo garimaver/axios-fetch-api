8562c9198755b76ab34ac2ff43b63bdd70893d18
garima verma <114860130+garimaver@users.noreply.github.com>
2024-06-13 13:45:13 +0530

    pass data uisng axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+  const [myData , setMyData] = useState([])
+
+  useEffect(() => {
+axios.get("https://jsonplaceholder.typicode.com/posts")
+.then((res)=> {
+  setMyData(res.data)
+})
+  },[])
   return (
-    <div>
-      hello fetch
-    </div>
+    <>{myData.map((post)=> {
+const {id, title, body} = post;
+return(
+  <div className='card' key={id}>
+     <h2>{title}</h2>
+     <p>{body}</p>
+  </div>
+)
+    })}
+  
+    </>
   )
 }
 
